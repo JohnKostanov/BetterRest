71c5a6311eed6ab8194fd1b61e1c34d0bc958abc
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2021-08-21 18:56:30 +0200

    Building a basic layout

---
+++ BetterREst/BetterREstApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterREstApp.swift
+//  BetterREst
+//
+//  Created by Джон Костанов on 20/8/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterREstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
